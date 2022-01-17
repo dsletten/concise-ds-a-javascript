12fe8df4fb22f2bf5a8804574e018e9902d11875
David Sletten <david@bosatsu.net>
2022-01-17 02:42:59 -0500

    Working on lists now.

--- containers.js
+++ containers.js
@@ -26,13 +26,13 @@
 //////////////////////////////////////////////////////////////////////////////
 "use strict";
 
+//
+//     Container
+//     
 function Container() {
     throw new Error("Cannot instantiate Container.");
 }
 
-//
-//    JavaScript DEFGENERIC
-//    
 Container.prototype.isEmpty = function() {
     throw new Error("Container does not implement isEmpty().");
 };
@@ -53,14 +53,9 @@ Dispenser.prototype = Object.create(Container.prototype);
 Dispenser.prototype.constructor = Dispenser;
 Object.defineProperty(Dispenser.prototype, "constructor", {enumerable: false, configurable: false});
 
-function Collection() {
-    throw new Error("Cannot instantiate Collection.");
-}
-
-Collection.prototype = Object.create(Container.prototype);
-Collection.prototype.constructor = Collection;
-Object.defineProperty(Collection.prototype, "constructor", {enumerable: false, configurable: false});
-
+//
+//    Node
+// 
 function Node(head, tail) {
     this.head = head;
     this.tail = tail;
@@ -150,3 +145,112 @@ Node.reverse = function(list) {
     return reverse(list, null);
 };
 
+//
+//     Collection
+//     
+function Collection() {
+    throw new Error("Cannot instantiate Collection.");
+}
+
+Collection.prototype = Object.create(Container.prototype);
+Collection.prototype.constructor = Collection;
+Object.defineProperty(Collection.prototype, "constructor", {enumerable: false, configurable: false});
+
+Collection.prototype.iterator = function() {
+    throw new Error("Collection does not implement iterator().");
+};
+
+//
+//     - Check type?
+//     - Equality test?
+//     
+Collection.prototype.contains = function(obj) {
+    throw new Error("Collection does not implement contains().");
+};
+
+Collection.prototype.equals = function(collection) {
+    throw new Error("Collection does not implement equals().");
+};
+
+Collection.prototype.each = function(op) {
+    throw new Error("Collection does not implement each().");
+};
+
+//
+//    Iterator
+//
+function Iterator() {
+    throw new Error("Cannot instantiate Iterator.");
+}
+
+Iterator.prototype.current = function() {
+    if ( this.isDone() ) {
+        throw new Error("Iteration already finished");
+    } else {
+        return this.doCurrent();
+    }
+};
+
+Iterator.prototype.doCurrent = function() {
+    throw new Error("Iterator does not implement doCurrent().");
+};
+
+Iterator.prototype.next = function() {
+    throw new Error("Iterator does not implement next().");
+};
+
+Iterator.prototype.done = function() {
+    throw new Error("Iterator does not implement done().");
+};
+
+//
+//    MutableCollectionIterator
+// 
+function MutableCollectionIterator(collection) {
+    this.collection = collection;
+    this.expectedModificationCount = collection.modificationCount;
+}
+
+MutableCollectionIterator.prototype = Object.create(Iterator.prototype);
+MutableCollectionIterator.prototype.constructor = MutableCollectionIterator;
+Object.defineProperty(MutableCollectionIterator.prototype, "constructor", {enumerable: false, configurable: false});
+
+MutableCollectionIterator.prototype.comodified = function() {
+    return this.expectedModificationCount !== this.collection.modificationCount;
+};
+
+MutableCollectionIterator.prototype.doCurrent = function() {
+    if ( this.comodified() ) {
+        throw new Error("Iterator invalid due to structural modification of collection");
+    } else {
+        return this.doDoCurrent();
+    }
+};
+
+MutableCollectionIterator.prototype.doDoCurrent = function() {
+    throw new Error("MutableCollectionIterator does not implement doDoCurrent().");
+};
+
+MutableCollectionIterator.prototype.next = function() {
+    if ( this.comodified() ) {
+        throw new Error("Iterator invalid due to structural modification of collection");
+    } else {
+        return this.doNext();
+    }
+};
+
+MutableCollectionIterator.prototype.doNext = function() {
+    throw new Error("MutableCollectionIterator does not implement doNext().");
+};
+
+MutableCollectionIterator.prototype.isDone = function() {
+    if ( this.comodified() ) {
+        throw new Error("Iterator invalid due to structural modification of collection");
+    } else {
+        return this.doIsDone();
+    }
+};
+
+MutableCollectionIterator.prototype.doIsDone = function() {
+    throw new Error("MutableCollectionIterator does not implement doIsDone().");
+};
--- list.js
+++ list.js
@@ -23,74 +23,316 @@
 //
 //   Notes:
 //
-//////////////////////////////////////////////////////////////////////////////
-"use strict";
-
-function List() {
-    throw new Error("Cannot instantiate List.");
-}
-
-List.prototype = Object.create(Container.prototype);
-List.prototype.constructor = List;
-Object.defineProperty(List.prototype, "constructor", {enumerable: false, configurable: false});
-
-List.prototype.addFront = function(obj) {
-    throw new Error("Abstract method.");
-};
-
-List.prototype.addRear = function(obj) {
-    throw new Error("Abstract method.");
-};
-
 //
 //    :AROUND method -> Template Method design pattern
 //    
-List.prototype.front = function() {
+//////////////////////////////////////////////////////////////////////////////
+"use strict";
+
+//
+//     List
+//     
+function List(fillElt) {
+    if ( fillElt === undefined ) {
+        this.fillElt = null;
+    } else {
+        this.fillElt = fillElt;
+    }
+}
+
+List.prototype = Object.create(Collection.prototype);
+List.prototype.constructor = List;
+Object.defineProperty(List.prototype, "constructor", {enumerable: false, configurable: false});
+
+// (defmethod print-object ((l list) stream)
+//   (print-unreadable-object (l stream :type t)
+//     (format stream "(")
+//     (loop with i = (iterator l)
+//           until (done i)
+//           do (format stream "~A" (current i))
+//              (next i)
+//              (unless (done i)
+//                (format stream " ")))
+//     (format stream ")")))
+
+// (defmethod equals ((l1 list) (l2 list) &key (test #'eql))
+//   (if (= (size l1) (size l2))
+//       (do ((i1 (iterator l1))
+//            (i2 (iterator l2)))
+//           ((and (done i1) (done i2)) t)
+//         (unless (funcall test (current i1) (current i2))
+//           (return nil))
+//         (next i1)
+//         (next i2))
+//       nil))
+
+// (defmethod each ((l list) op)
+//   (let ((i (iterator l)))
+//     (loop until (done i)
+//           do (funcall op (current i))
+//              (next i))))
+
+List.prototype.listIterator = function(start) {
+    throw new Error("List does not implement listIterator().");
+};
+
+List.prototype.add = function(...objs) {
+    throw new Error("List does not implement add().");
+};
+
+List.prototype.extendList = function(i, obj) {
+    let elts = new Array(i - this.size()).fill(this.fillElt);
+    elts.push(obj);
+    this.add(...elts);
+};
+
+List.prototype.insert = function(i, obj) {
+    if ( i < 0 ) {
+        let j = i + this.size();
+        if ( j >= 0 ) {
+            this.insert(j, obj);
+        }
+    } else if ( i >= this.size() ) {
+        this.extendList(i, obj);
+    } else {
+        this.doInsert(i, obj);
+    }
+};
+
+List.prototype.doInsert = function(i, obj) {
+    throw new Error("List does not implement doInsert().");
+};
+
+List.prototype.delete = function(i) {
     if ( this.isEmpty() ) {
         throw new Error("List is empty.");
+    } else if ( i < 0 ) {
+        let j = i + this.size();
+        if ( j >= 0 ) {
+            return this.delete(j);
+        }
+    } else if ( i < this.size() ) {
+        return this.doDelete(i);
+    }
+};
+
+List.prototype.doDelete = function(i) {
+    throw new Error("List does not implement doDelete().");
+};
+
+List.prototype.get = function(i) {
+    if ( i < 0 ) {
+        let j = i + this.size();
+        if ( j < 0 ) {
+            return null;
+        } else {
+            return this.get(j);
+        }
+    } else if ( i >= this.size() ) {
+        return null;
     } else {
-        return this.doFront();
+        return this.doGet(i);
     }
 };
 
-List.prototype.rear = function() {
-    if ( this.isEmpty() ) {
-        throw new Error("List is empty.");
+List.prototype.doGet = function(i) {
+    throw new Error("List does not implement doGet().");
+};
+
+List.prototype.set = function(i, obj) {
+    if ( i < 0 ) {
+        let j = i + this.size();
+        if ( j >= 0 ) {
+            this.set(j, obj);
+        }
+    } else if ( i >= this.size() ) {
+        this.extendList(i, obj);
     } else {
-        return this.doRear();
+        this.doSet(i, obj);
     }
 };
 
-List.prototype.removeFront = function() {
-    if ( this.isEmpty() ) {
-        throw new Error("List is empty.");
+List.prototype.doSet = function(i) {
+    throw new Error("List does not implement doSet().");
+};
+
+//   Type, test?
+List.prototype.index = function(obj) {
+    throw new Error("List does not implement index().");
+};
+
+List.prototype.slice = function(i, n) {
+    if ( n < 0 ) {
+        throw new Error(`Count \`n\` must be non-negative: ${n}`);
+    } else if ( i < 0 ) {
+        let j = i + this.size();
+        if ( j < 0 ) {
+            return this.slice(0, 0);
+        } else {
+            return this.slice(j, n);
+        }
     } else {
-        this.doRemoveFront();
+        return this.doSlice(i, n);
     }
 };
 
-List.prototype.removeRear = function() {
-    if ( this.isEmpty() ) {
-        throw new Error("List is empty.");
+List.prototype.doSlice = function(i, n) {
+    throw new Error("List does not implement doSlice().");
+};
+
+//
+//     MutableList
+//     
+function MutableList(fillElt) {
+    List.call(this, fillElt);
+    this.modificationCount = 0;
+}
+
+MutableList.prototype = Object.create(List.prototype);
+MutableList.prototype.constructor = MutableList;
+Object.defineProperty(MutableList.prototype, "constructor", {enumerable: false, configurable: false});
+
+MutableList.prototype.clear = function() {
+    this.countModification();
+    this.doClear();
+};
+
+MutableList.prototype.doClear = function() {
+    throw new Error("MutableList does not implement doClear().");
+};
+
+MutableList.prototype.countModification = function() {
+    this.modificationCount++;
+};
+
+MutableList.prototype.add = function(...objs) {
+    this.countModification();
+    this.doAdd(...objs);
+};
+
+MutableList.prototype.doAdd = function(...objs) {
+    throw new Error("MutableList does not implement doAdd().");
+};
+
+MutableList.prototype.doInsert = function(i, obj) {
+    this.countModification();
+    this.doDoInsert(i, obj);
+};
+
+MutableList.prototype.doDoInsert = function(i, obj) {
+    throw new Error("MutableList does not implement doDoInsert().");
+};
+
+MutableList.prototype.doDelete = function(i) {
+    this.countModification();
+    return this.doDoDelete(i);
+};
+
+MutableList.prototype.doDoDelete = function(i) {
+    throw new Error("MutableList does not implement doDoDelete().");
+};
+
+//
+//    ArrayList
+// 
+function ArrayList(fillElt) {
+    MutableList.call(this, fillElt);
+    this.store = [];
+}
+
+ArrayList.prototype = Object.create(MutableList.prototype);
+ArrayList.prototype.constructor = ArrayList;
+Object.defineProperty(ArrayList.prototype, "constructor", {enumerable: false, configurable: false});
+
+ArrayList.prototype.size = function() {
+    return this.store.length;
+};
+
+ArrayList.prototype.isEmpty = function() {
+    return this.store.length === 0;
+};
+
+ArrayList.prototype.iterator = function() {
+    return new RandomAccessListIterator(this);
+};
+    
+    // def list_iterator(start=0)
+    //   RandomAccessListListIterator.new(self, start)
+    // end
+
+ArrayList.prototype.doClear = function() {
+    this.store = [];
+};
+
+ArrayList.prototype.contains = function(obj) {
+    if ( this.store.find(x => x === obj) === undefined ) {
+        return false;
     } else {
-        this.doRemoveRear();
+        return obj;
     }
 };
 
-List.prototype.isEmpty = function() {
-    return this.size() === 0;
+ArrayList.prototype.doAdd = function(...objs) {
+    this.store = this.store.concat(objs);
 };
 
-List.prototype.makeEmpty = function() {
-    while ( !this.isEmpty() ) {
-        this.removeFront();
+ArrayList.prototype.doDoInsert = function(i, obj) {
+    this.store.splice(i, 0, obj);
+};
+
+ArrayList.prototype.doDoDelete = function(i) {
+    return this.store.splice(i, 1)[0];
+};
+
+ArrayList.prototype.doGet = function(i) {
+    return this.store[i];
+};
+
+ArrayList.prototype.doSet = function(i, obj) {
+    this.store[i] = obj;
+};
+
+ArrayList.prototype.index = function(obj) {
+    return this.store.findIndex(x => x === obj);
+};
+
+ArrayList.prototype.doSlice = function(i, n) {
+    let list = new ArrayList(this.fillElt);
+    list.add(...this.store.slice(i, i + n));
+
+    return list;
+};
+
+//
+//     RandomAccessListIterator
+//
+function RandomAccessListIterator(collection) {
+    MutableCollectionIterator.call(this, collection);
+    this.cursor = 0;
+}
+
+RandomAccessListIterator.prototype = Object.create(MutableCollectionIterator.prototype);
+RandomAccessListIterator.prototype.constructor = RandomAccessListIterator;
+Object.defineProperty(RandomAccessListIterator.prototype, "constructor", {enumerable: false, configurable: false});
+
+RandomAccessListIterator.prototype.doDoCurrent = function() {
+    return this.collection.get(this.cursor);
+};
+
+RandomAccessListIterator.prototype.doNext = function() {
+    if ( this.isDone() ) {
+        return null;
+    } else {
+        this.cursor++;
+
+        if ( this.isDone() ) {
+            return null;
+        } else {
+            return this.current();
+        }
     }
 };
 
-List.prototype.elements = function() {
-    throw new Error("Abstract method.");
-};
-
-List.prototype.contains = function() {
-    throw new Error("Abstract method.");
+RandomAccessListIterator.prototype.doIsDone = function() {
+    return this.cursor === this.collection.size(); // >=
 };
